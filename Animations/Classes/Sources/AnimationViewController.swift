8b557278941796aa4df1c2874c1a72da71064021
Renilson Moreira <renilsonmoreira_@hotmail.com>
2022-07-18 17:17:27 -0300

    ajustes para build no modulo

---
+++ Animations/Classes/Sources/AnimationViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Renilson Moreira on 09/07/22.
+//
+
+import UIKit
+
+public class AnimationViewController: UIViewController {
+
+    let contentView = MyAnimationView()
+    
+    public override func loadView() {
+        super.loadView()
+        self.view = contentView
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        contentView.backMainAppButton.addTarget(self, action: #selector(didTapMainAppBackButton), for: .touchUpInside)
+            
+        playAnimation()
+    }
+    
+    @objc func didTapMainAppBackButton() {
+        navigationController?.popToRootViewController(animated: true)
+    }
+
+    func playAnimation() {
+        self.contentView.animationView.play { played in
+            if played == true {
+                print("Navigate here")
+            }else {
+                self.playAnimation()
+            }
+        }
+    }
+
+
+}
+
+
---
+++ Animations/Classes/Sources/MyAnimationView.swift
@@ -0,0 +1,57 @@
+//
+//  MyAnimationView.swift
+//  Animations
+//
+//  Created by Renilson Moreira on 09/07/22.
+//
+
+import UIKit
+import Lottie
+
+class MyAnimationView: UIView {
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        setupView()
+        self.backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var animationView: AnimationView = {
+        let animation = AnimationView(name: "saved")
+        animation.contentMode = .scaleAspectFit
+        animation.translatesAutoresizingMaskIntoConstraints = false
+        animation.loopMode = .loop
+        animation.play()
+        return animation
+    }()
+    
+    lazy var backMainAppButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Voltar para app principal", for: .normal)
+        button.backgroundColor = .orange
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    func setupView() {
+        addSubview(animationView)
+        addSubview(backMainAppButton)
+        
+        NSLayoutConstraint.activate([
+            animationView.topAnchor.constraint(equalTo: topAnchor, constant: 50),
+            animationView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            animationView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            animationView.heightAnchor.constraint(equalToConstant: 200),
+            
+            backMainAppButton.topAnchor.constraint(equalTo: animationView.bottomAnchor, constant: 50),
+            backMainAppButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            backMainAppButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            backMainAppButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+}
